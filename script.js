aec8ae3e2652bb56d5687d9b1677228596742c92
Eltorshito <cedricg697@gmail.com>
2023-09-28 13:59:48 +0200

    finish

--- script.js
+++ script.js
@@ -23,18 +23,27 @@ addTaskButton.addEventListener('click', addTask);
 
 
 function deleteOrCompleteTask(event) {
-    const target = event.target;
-    const taskItem = target.closest('li');
-  
-    if (target.classList.contains("done")) {
-      taskItem.classList.toggle("completed");
+  const target = event.target;
+  const taskItem = target.closest('li');
+  const paragraph = taskItem.firstElementChild;
+
+  if (target.classList.contains("done")) {
+      paragraph.classList.add("completed");
       target.textContent = "Undo";
-    } else if (target.classList.contains("delete")) {
+      target.classList.remove("done");
+      target.classList.add("undo");
+  } else if (target.classList.contains("undo")) {
+      paragraph.classList.remove("completed");
+      target.textContent = "Done";
+      target.classList.remove("undo");
+      target.classList.add("done");
+  } else if (target.classList.contains("delete")) {
       taskItem.remove();
-    }
-  };
-  
-  taskListElement.addEventListener('click', deleteOrCompleteTask);
+  }
+}
+
+taskListElement.addEventListener('click', deleteOrCompleteTask);
+
 
 
   
